cb4ceaa3b55a18048de333ad1e40089227fdfb3f
matulni <m.uldemolins@gmail.com>
2025-12-31 17:19:36 +0100

    Finished chapter 3

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,69 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    pub product_name : String,
+    pub quantity : i32,
+    pub unit_price : i32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: i32, unit_price: i32) -> Order{
+        Order::check_product_name(&product_name);
+        Order::check_quantity(quantity);
+        Order::check_unit_price(unit_price);
+        Order{product_name, quantity, unit_price}
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &i32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &i32 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        Order::check_product_name(&product_name);
+        self.product_name = product_name;
+    }
+
+    pub fn set_quantity(&mut self, quantity: i32) {
+        Order::check_quantity(quantity);
+        self.quantity = quantity;
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: i32) {
+        Order::check_unit_price(unit_price);
+        self.unit_price = unit_price;
+    }
+
+    pub fn total(&self) -> i32 {
+        *self.unit_price() * *self.quantity()
+    }
+
+    fn check_product_name(product_name: &String) {
+        if product_name.is_empty() {
+            panic!("Product name cannot be empty");
+        }
+        if product_name.len() > 300 {
+            panic!("Product name cannot be longer than 500 bytes");
+        }
+
+    }
+    fn check_quantity(q: i32){
+        if q <= 0 {
+            panic!("Quantity must be larger than 0");
+        }
+    }
+    fn check_unit_price(p: i32){
+        if p <= 0 {
+            panic!("Unit price must be larger than 0");
+        }
+    }
+}
